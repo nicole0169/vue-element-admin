aa81ba9f2f432ec741a60fa30ec50ac7cf2c2b33
miaomin <miaomin@bitmap3d.com.cn>
2019-01-29 16:59:06 +0800

    Simple Form submit testing.

--- src/views/form/simpleForm/index.vue
+++ src/views/form/simpleForm/index.vue
@@ -13,12 +13,12 @@
       <el-form-item label="活动时间">
         <el-col :span="11">
           <el-date-picker type="date" v-model="simpleForm.date1" placeholder="选择日期"
-                          style="width: 100%;"></el-date-picker>
+                          style="width: 100%;" value-format="yyyy-MM-dd"></el-date-picker>
         </el-col>
         <el-col class="line" :span="2">-</el-col>
         <el-col :span="11">
           <el-time-picker type="fixed-time" v-model="simpleForm.date2" placeholder="选择时间"
-                          style="width: 100%;"></el-time-picker>
+                          style="width: 100%;" value-format="HH:mm:ss"></el-time-picker>
         </el-col>
       </el-form-item>
       <el-form-item label="即时配送">
@@ -26,16 +26,16 @@
       </el-form-item>
       <el-form-item label="活动性质">
         <el-checkbox-group v-model="simpleForm.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+          <el-checkbox label="1" name="type">美食/餐厅线上活动</el-checkbox>
+          <el-checkbox label="2" name="type">地推活动</el-checkbox>
+          <el-checkbox label="3" name="type">线下主题活动</el-checkbox>
+          <el-checkbox label="4" name="type">单纯品牌曝光</el-checkbox>
         </el-checkbox-group>
       </el-form-item>
       <el-form-item label="特殊资源">
         <el-radio-group v-model="simpleForm.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
+          <el-radio label="1">线上品牌商赞助</el-radio>
+          <el-radio label="2">线下场地免费</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="活动省市">
@@ -89,6 +89,8 @@
         this.$refs[formName].validate((valid) => {
           if (valid) {
             this.loading = true
+            //整理数据
+            this.simpleForm.type = JSON.stringify(this.simpleForm.type)
             //开始提交表单
             new Promise((resolve, reject) => {
               submitSimpleForm(this.simpleForm).then(response => {
